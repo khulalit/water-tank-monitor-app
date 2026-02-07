45ffa99d6ac3f21b628c9c863624e82217b3217f
LalitKhu <lalitkhudhania2@gmail.com>
2026-02-07 17:30:53 +0530

    added vercel json

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router";
 import SensorOffline from "../components/sensor-offline";
 import WaterTankMonitor from "../components/water-tank-moniter";
+import { useAuth } from "../context/auth-context";
 import { useTank } from "../context/tank-data";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    navigate("/login");
+  }
+
   const { percentage, volume, alive, error, reconnecting } = useTank();
 
   // Connection lost
